f98f0c88b605dc260a24b3ca33fb77d5725e3f7b
Musfiquer Rhman <musfiquerrhman@gmail.com>
2023-06-02 20:57:56 +0600

    future only date in coupons

--- src/Pages/cupons/cupons.jsx
+++ src/Pages/cupons/cupons.jsx
@@ -59,7 +59,7 @@ const Cupons = () => {
     }
 
     let date = new Date();
-    let day = date.getDate();
+    let day = date.getDate() + 1;
     if(day < 10) day = `0${day}`;
     let month = date.getMonth() + 1;
     if(month < 10) month = `0${month}`;
